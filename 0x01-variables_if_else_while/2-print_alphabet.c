920e65b0a6a2b7edcdc7821f2cbcc92be36eb99c
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-06 09:26:49 -0700

    print alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++)
+	for(low = 'a'; low <= 'z'; low++)
 		putchar(low);
 
-	        putchar('\n');
+        putchar('\n');
 
-		return(0);
+	return(0);
 }
