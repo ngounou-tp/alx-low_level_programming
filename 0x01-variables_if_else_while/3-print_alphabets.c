ff6ca4e2e0930ebb7ac868177583206045ed1108
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-06 22:17:15 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,8 +12,8 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a'; low <= 'z'; low++);
-          	putchar(low);
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
 	for (low = 'A'; low <= 'Z'; low++)
 		putchar(low);
 	putchar('\n');
