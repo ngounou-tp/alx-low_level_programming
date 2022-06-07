7ed7ab683d3aee0ab3c060e7232bf1f16168f3b0
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-06 23:00:50 -0700

    alphabet with exception

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,18 +4,19 @@
 
 /**
  * main - Entry point
- *   
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	char low, e, q;
+
 	e = 'e';
 	q = 'q';
 
 	for (low = 'a'; low <= 'z'; low++)
-	{										
+	{
 		if (low != e && low != q)
 			putchar(low);
 	}
