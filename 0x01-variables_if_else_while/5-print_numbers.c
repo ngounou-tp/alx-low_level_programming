cd9d3673d614551454d42efffd06dbb197774b22
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-07 08:28:11 -0700

    print number

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,11 @@
-include<stdio.h>
-include<stdlib.h>
-include<time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 /**
  * main - Entry point
  *
- * Always: 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
