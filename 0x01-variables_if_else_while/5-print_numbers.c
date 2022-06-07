8e022900022bd14cd08a5713f2d5ec66138d2969
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-07 08:22:09 -0700

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+include<stdio.h>
+include<stdlib.h>
+include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Always: 0 (Success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for(a=0; a<=9; a++)
+		printf("%d", a);
+	printf("\n");
+
+	return (0);
+}
