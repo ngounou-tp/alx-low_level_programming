0b64566a541740d028a9c3e3349883aabdb37b14
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-07 08:51:18 -0700

    print number v2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		putchar(c);
+	putchar('\n');
+
+	return(0);
+}
+
