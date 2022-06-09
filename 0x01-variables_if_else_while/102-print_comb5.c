bcd29e63976ddd5b1ebdd52df40360d5774a7ac0
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-09 08:11:24 -0700

    speciale combinaision

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,13 +18,13 @@ int main(void)
 		{
 			putchar((p / 10) + '0');
 			putchar((p % 10) + '0');
-			putchar(' ');			
+			putchar(' ');
 			putchar((q / 10) + '0');
 			putchar((q % 10) + '0');
-			
+
 			if (p == 98 && q == 99)
 				continue;
-		
+
 			putchar(',');
 			putchar(' ');
 		}
