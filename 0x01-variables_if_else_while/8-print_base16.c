dfe31c5c691644d6f190f2334d3a67f6db342712
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-09 04:29:32 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+include<stdio.h>
+include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int c;
+	char low;
+
+	for (c = '0'; c < '9'; c++)
+		putchar(c);
+	for (low = a; low < g; low++)
+		putchar(a);
+	putchar('\n');
+
+	return (0);
+}
