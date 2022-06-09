e5a797176675b2ca7188cafba438048fa1e7ddc9
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-09 05:40:28 -0700

    numbers order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0';  c <= '9'; c++)
+	{
+		putchar(c);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
+	 	
