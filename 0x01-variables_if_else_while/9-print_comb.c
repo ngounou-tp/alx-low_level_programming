88fa2f63adc471473e4a25d32349e72ef3539113
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-09 07:15:18 -0700

    digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 {
 	int c;
 
-	for (c = '0';  c < '9'; c++)
+	for (c = '0';  c <= '9'; c++)
 	{
 		putchar(c);
-		putchar(',');
-		putchar(' ');
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
-	putchar('9');
 
 	return (0);
 }
