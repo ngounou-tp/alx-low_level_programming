10dd85426fe1b53139e76286ce85cc1b8c40a048
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-11 00:24:04 -0700

    print alphabetx10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,10 +11,10 @@ void print_alphabet_x10(void)
 	int a;
 	char c;
 
-	for (a = 0; a < 10; a++){
-		for (c = 'a'; c < 'z'; c++){
+	for (a = 0; a < 10; a++) {
+		for (c = 'a'; c < 'z'; c++) {
 			_putchar(c);
-			_putchar('\n');
 		}
+		_putchar('\n')
 	}
 }
