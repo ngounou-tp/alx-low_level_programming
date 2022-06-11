439eec8a0d60e1228593fada1ceb9aec13613e1a
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-10 22:42:29 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,18 +15,6 @@ int _putchar(char c)
 	return (write(1, &c, 1));
 }
 
-int main(void)
-{
-	_putchar(_);
-	_putchar(p);
-	_putchar(u);
-	_putchar(t);
-        _putchar(c);
-	_putchar(h);
-	_putchar(a);
-	_putcha(r);
-        _putchar('\n');
-}
 
 
 
