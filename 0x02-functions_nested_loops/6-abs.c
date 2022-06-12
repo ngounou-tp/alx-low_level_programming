540223645e5b24041d633458bd8f2b66446e4fb8
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-11 20:00:16 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absolude value
+ * @r: An integre input
+ * Description: This function returns absolute value of a number
+ * Return: Absolut value of number r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+	
+		return (r);
+	else
+		return (r * -1);
+}
