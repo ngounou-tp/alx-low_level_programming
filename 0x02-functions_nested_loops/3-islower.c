a08b727e7af76f8110269699b28f33c33ad16c87
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-11 18:38:10 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower -check if a character is in lower case
+ * Input :an interger
+ *
+ * Retur: if true return 1 and 0 if false
+ */
+
+int _islower(int c)
+{
+	char s;
+	int t = 0;
+
+	for (s = 'a''; s <= 'z'; s++)
+	{
+		if (s == a)
+			t = 1;
+	}
+
+	return (t);
+}
