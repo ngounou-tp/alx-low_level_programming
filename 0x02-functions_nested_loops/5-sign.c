3ff91a303ba6655788b30bcbef4602c5add7a433
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-11 19:19:24 -0700

    check sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_sign - prints signs
+ * @n: An input integer
+ * Description: function uses _putchar function to print signs
+ * Return: 1 if positif 0if equal to zero or -1 if negatif
+ */
+ 
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+
+		return (1);
+	}
+	else 
+		if (n == 0)
+		{
+			_putchar(0);
+
+			return (0);
+		}
+		else 
+		{
+			_putchar(-);
+
+			return (-1);
+		}
+}
