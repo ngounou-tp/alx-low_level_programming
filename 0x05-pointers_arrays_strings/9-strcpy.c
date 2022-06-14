f4bd70263bdb787686ff843659e9281b4558c759
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-14 16:55:08 -0700

    string copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - to be tested by main.c
+ * @dest: array of characters
+ * @src: array of characters
+ * Description: copies the first string to the second string
+ * Return: pointer to the first string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *c;
+
+	*c = *dest;
+	while (*src) //ne pointe pas vers le vide
+		*dest++ = *src;
+
+	return (c);
+}
