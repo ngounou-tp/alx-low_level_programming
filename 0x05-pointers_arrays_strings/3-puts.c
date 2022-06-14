081f68a812954d5c6dcfc7fa42786bbd43fb86e2
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-14 12:16:44 -0700

    print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *  _puts - tested by main.c file
+ *  @s: pointer to a string
+ *  Description: prints a string
+ *  Return: nothing
+ */
+
+void _puts(char *str)
+{
+	char c;
+	int i;
+
+	c = s[0];
+	i = 0;
+
+	while (c != '\0')
+	{
+		_putchar(c);
+		i++;
+		c = s[i];
+	}
+}
