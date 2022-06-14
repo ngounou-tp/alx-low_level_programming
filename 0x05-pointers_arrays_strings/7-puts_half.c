ab108b8715bfc3dd1101650fd4ed6e735c78cde5
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-14 15:46:48 -0700

    print half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,7 +2,7 @@
 
 /**
  * puts_half - tested by main.c file
- * @s: array of character
+ * @str: array of character
  * Description: print half number of charater
  * Return: nothing
  */
@@ -18,14 +18,13 @@ void puts_half(char *str)
 		i++;
 		c = str[i];
 	}
+       	if (i % 2 == 0)
+       		j = i / 2;
+	else
+		j = (i - 1) / 2;
 
-	 if ( i % 2 == 0)
-		 j = i / 2;
-	 else
-		 j = (i - 1) / 2;
-
-	 for (k = j; k < i; k++)
-		 _putchar(str[k]);
-	 _putchar('\n');
+	for (k = j - 1; k < i; k++)
+		_putchar(str[k]);
+	_putchar('\n');
 }
 
