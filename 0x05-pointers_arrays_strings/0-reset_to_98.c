3d6af6fc65d4ad188a0e4089e19f86503327645a
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-14 11:35:13 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - to be tested by main.h
+ * @n: pointer to and input
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+readme file for pointers arrays and string
