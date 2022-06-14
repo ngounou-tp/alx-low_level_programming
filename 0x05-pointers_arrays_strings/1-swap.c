0fb249b18e52ee8034607c5e91fdc876ffd739d5
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-14 11:44:22 -0700

    swapping intergers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - tested by the main.c file
+ * @a: pointer to and integer
+ * @b: pointer to and interger
+ * Description: swaps thw value of two given interger
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	int *c;
+
+	c = &a;
+	a = &b;
+	b = &c;
+}
