857da062627ffc08742bad0b8adae86de2a7f6a4
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-16 16:09:32 -0700

    string concate n

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,10 @@
-#include "main.h"
-
 /**
  * _strncat - a function that concatenates two strings.
  * @src: an input string
  * @n: an input integer
  * Return: A pointer to the resulting string
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int srclen = 0, i = 0;
