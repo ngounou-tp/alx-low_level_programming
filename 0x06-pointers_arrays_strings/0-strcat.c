76577a2f531b796fadd3dd95bf46ba6cc0ae440e
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-16 15:33:04 -0700

    string concate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,16 @@ char *_strcat(char *dest, char *src)
 {
 	char *temp = dest;
 	
-	while (*dest)
+	while (*dest != '\0')
 		dest++;
 	
-	while (*src)
-		*dest++ = *src++;
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+
 
 	*dest++ = '\0';
 	return (temp);
