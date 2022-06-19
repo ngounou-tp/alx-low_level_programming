7314afe2c95cd02e600bae2e18a300b21a463eac
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-19 07:49:43 -0700

    print n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,7 +9,7 @@ void print_times_table(int n)
 {
 	int i, j;
 
-	if (n > 0 && n < 15)
+	if (n < 15 && n > 0)
 	{
 		for (i = 0; i <= n; i++)
 		{
@@ -19,7 +19,6 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	else
 }
 /**
  * putformat - formatted characters to output
