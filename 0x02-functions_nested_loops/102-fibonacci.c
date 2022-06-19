a4f56cdec84e17fc806e0535aa89b45e0cbe0de3
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-19 10:39:27 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int i = 0, j = 1, n = 0;
+	int i = 0, j = 1, n;
 
 	printf("%d", i);
 	putchar(',');
@@ -15,7 +15,7 @@ int main(void)
 	printf("%d", j);
 	putchar(',');
 	putchar(' ');
-	while (n >= 2 && n <= 50)
+	while (n <= 50)
 	{
 		n = i + j;
 		printf("%d", n);
