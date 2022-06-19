ec334327d2cc42e4b9a446c411f38ba5527615ba
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-19 08:41:14 -0700

    multiples add

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0, j = 0, k = 0;
+
+	while (i < 1024)
+	{
+		if ( j * i % 3 == 0 || j * i % 5 == 0)
+			k = k + j ;
+		i++;
+	}
+	printf("%d", k);
+
+	return (0);
+}
+
