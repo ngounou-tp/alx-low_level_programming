7f98ea7fd033fedd7f24be073380a2899f947657
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-21 04:05:35 -0700

    search c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+/**
+ * _strchr - find a character in a string
+ * @s: input string
+ * @c: input character to look for
+ * Description: find the first occcurence of c in s
+ * Return: a poiner to that first  occurence
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0, j = 0;
+
+	while(*s)
+		i++;
+	while (j < i)
+	{
+		if (*s == c)
+			return (s);
+		s++;
+		j++;
+	}
+
+	return (NULL);
+}
+		
