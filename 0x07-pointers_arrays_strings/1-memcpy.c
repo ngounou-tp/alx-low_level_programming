ba6680e5fad715dc12f296911158ed6c9ef48829
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-21 03:42:24 -0700

    memory copy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * _memcpy: copy the  n bits of a given memory adress
+ * @dest: the memory adres to which we copy
+ * @src: the memory address from which we copy
+ * @n: n bits to be copied
+ * Description: copy the n bits of src to dest
+ * Return: the address of dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *c = dest;
+
+	while (n--)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	
+	return (c);
+}
