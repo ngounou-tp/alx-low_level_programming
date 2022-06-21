ed12036752b719ced1e19edfadaaac8a1eb27ff8
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-21 03:32:34 -0700

    assignment to pointer

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -7,6 +7,8 @@
  * @s: pointer to be assigned
  * @b: the constant byte to be assigned
  * @n: the n byte to be assigned
+ * Description: assigned the n bits of s to b
+ * Return: the adress of the pointer
  */
 
 char *_memset(char *s, char b, unsigned int n)
