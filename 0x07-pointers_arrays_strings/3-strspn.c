1a3b9e4293128c5c74e3023b7028b595fef3be8a
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-21 06:24:19 -0700

    number of characters

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _strspn - count the number of character of a given string
+ * @s: input string to do the test
+ * @accepted: input string conatining characters to be tested
+ * Description: counts number of characters of accept that are in s
+ * Return: interger
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	
+	while (*s)
+	{
+		while (*accept)
+		{
+			if (*s == *accept)
+			{
+				i++;
+				accept++;
+			}
+			s++;
+		}
+	}
+	if (*s == *accept)
+		i++;
+	
+	return (i);
+}
