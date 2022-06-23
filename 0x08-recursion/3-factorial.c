112fd257b65ccaec32cf3e7e916ecc9ba336df63
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-23 16:54:23 -0700

    factoriall

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,15 +3,18 @@
 /**
  * factorial - a function that returns the factorial
  * @n: input interger
- * Description: a function that returns 
+ * Description: a function that returns
  * the factorial of a given number
  * Return: interger
  */
 int factorial(int n)
 {
+	if (n < 0)
+		return -1
 	if (n == 0)
 		return 1;
 	if (n == 1)
 		return 1;
-	return(n*factorial(n - 1));
+	if (n > 1)
+		return(n * factorial(n - 1));
 }
