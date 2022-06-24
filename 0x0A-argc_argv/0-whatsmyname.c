84be34352db1d096c1483ef63c9fe584ea2ee8b0
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-24 06:44:33 -0700

    file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ * @argc: number of argument pass to the command line
+ * @argv[]: liste of argument
+ * Description: prints the name of the program
+ * Return: always 0
+ */
+int main?(int argc, char *argv[])
+{
+	printf("%s/n", argv[0]));
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+0-whatsmyname.c : a program that prints its name, followed by a new line
