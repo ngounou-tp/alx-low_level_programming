3c494ca7310e7b129505a904a10864192ecd01c5
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-23 17:50:18 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - calculate natural squqre root
+ * @n: input integer
+ * Description: a function that returns 
+ * the natural square root of a number.
+ * Return: interger
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - find square root
+ * @prev: previous value
+ * @root: square root value
+ * Return: the square root
+ */
+int _sqrt(int prev, int root)
+{
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -3,3 +3,4 @@ void _print_rev_recursion(char *s): a function that prints a string in reverse.
 _strlen_recursion(char *s): a function that returns the length of a string
 factorial(int n): a function that returns the factorial of a given number.
 _pow_recursion(int x, int y): a function that returns the value of x raised to the power of y.
+int _sqrt_recursion(int n): a function that returns the natural square root of a number
