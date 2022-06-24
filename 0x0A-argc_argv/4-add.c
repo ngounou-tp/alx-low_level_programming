e1404d04565299411328cac75a668159e2debf4d
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-24 10:02:05 -0700

    add digit

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - adds argument
+ * @argc: number of argument
+ * @argv: liste of argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num, result = 0, i;
+
+	while (argc-- > 1)
+	{
+		for (i = 0; argv[argc][i]; i++)
+		{
+			if (!(isdigit(argv[argc][i])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[argc]);
+		result += num;
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
