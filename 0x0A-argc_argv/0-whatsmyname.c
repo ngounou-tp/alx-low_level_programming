f2079940c05d1b60f8530453048bbaa0be7fdb85
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-24 06:50:07 -0700

    whats my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,9 +7,9 @@
  * Description: prints the name of the program
  * Return: always 0
  */
-int main?(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s/n", argv[0]));
+	printf("%s/n", argv[0]);
 
 	return (0);
 }
