693eff6739299569ba99b7c6a624ad7bd9d48b50
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-23 17:19:14 -0700

    fpower x to yclear

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - raise x to the power y
+ * @x: input integer
+ * @y: input integer
+ * Description: returns the value 
+ * of x raised to the power of y
+ * Return: interger
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	if (y == 1)
+		return (x);
+	if (x == 0)
+		return 0;
+	return (x * _pow_recursion(x, y - 1));
+}
