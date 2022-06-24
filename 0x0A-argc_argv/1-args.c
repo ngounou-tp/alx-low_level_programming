8132f679c2e2f52454d0613ca4d596b988ef64a9
ngounou-tp <patriciatchuinteu@gmail.com>
2022-06-24 07:38:09 -0700

    number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - print the number of argument pass into it
+ * @argc: number of argument
+ * @argv: list of argument
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
