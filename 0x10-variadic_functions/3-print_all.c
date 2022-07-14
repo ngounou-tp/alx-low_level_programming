5eb823d84c4990ae195a32bad118147f299bbd16
ngounou-tp <patriciatchuinteu@gmail.com>
2022-07-13 23:28:15 -0700

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,7 +14,7 @@ void print_all(const char * const format, ...)
 	va_list ap;
 	char *temp;
 	int i;
-
+	
 	va_start(ap, format);
 	while (format == NULL)
 	{
@@ -44,7 +44,7 @@ void print_all(const char * const format, ...)
 				printf("%s", temp);
 				break;
 		}
-		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||                format[i] == 's') && format[(i + 1)] != '\0')
+		if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f' ||format[i] == 's') && format[(i + 1)] != '\0')
 			printf(", ");
 		i++;
 	}
