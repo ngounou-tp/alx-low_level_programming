3be925e2eca94400a857611e2c0a7d70137aeb13
ngounou-tp <patriciatchuinteu@gmail.com>
2022-07-18 11:50:13 +0100

    print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,4 +7,20 @@
  * @h: input list
  * Return: size of the list
  */
+size_t print_list(const list_t *h)
+{
+	unsigned int nodes = 0;
 
+	while (h)
+	{
+		printf("[%u] ", h->len);
+		if (h->str == NULL)
+			printf("(nil)\n");
+		else
+			printf("%s\n", h->str);
+
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
