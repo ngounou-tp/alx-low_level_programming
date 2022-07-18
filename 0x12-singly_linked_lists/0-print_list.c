488a0f9b3ed8a35edd40078bcef1de7ec8583ac6
ngounou-tp <patriciatchuinteu@gmail.com>
2022-07-18 03:45:28 -0700

    creating files

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * print_list - print the elements of a list
+ * @h: input list
+ * Return: size of the list
+ */
+
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+0-print_list.c: Write a function that prints all the elements of a list_t list.
